8296bd8116b1b6889f88d5cb381686b3dbfa70bf
rubedo <rubedoje@gmail.com>
2014-09-02 00:31:20 -0400

    Removed views/lib because it was redundant.

--- _ddoc/views/lib/helpers.js
+++
@@ -1,8 +0,0 @@
-exports.require = function (doc, fieldlist) {
-  fieldlist.forEach(function (elem) {
-    var message = elem + ' is required';
-    if(!doc[elem]) {
-      throw({ 'forbidden': message });
-    }
-  });
-}
